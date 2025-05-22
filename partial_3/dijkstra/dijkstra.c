805f572466c7e6dd10a1aa4226fba33a1fa5674a
juanQNav <juan.qn363@gmail.com>
2025-05-22 02:51:34 -0600

    sequential dijkstra

---
+++ partial_3/dijkstra/dijkstra.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N_VERTICES 7
+#define INF 99999
+#define NAME_LEN 1
+#define MAX_WEIGHT 11
+
+typedef struct {
+  int id;
+  char nombre[20];
+}Vertice;
+
+typedef struct{
+  Vertice vertices[N_VERTICES];
+  int matriz_adyacencia[N_VERTICES][N_VERTICES];
+}Grafo;
+
+void nombra_vertices(Grafo *g, int num_vertices){
+  for(int i = 0; i < num_vertices; i++){
+    g->vertices[i].id = i;
+
+    for (int j = 0; j < NAME_LEN; j++){
+      g->vertices[i].nombre[j] = '0' + i;
+    }
+    g->vertices[i].nombre[NAME_LEN] = '\0';
+  }
+}
+
+void llena_matriz_a(Grafo *g, int num_vertices){
+  for(int i = 0; i < num_vertices; i++){
+    for(int j = 0; j < num_vertices; j++){
+      if(i == j){
+        g->matriz_adyacencia[i][j] = 0;
+      } else {
+        g->matriz_adyacencia[i][j] = rand() % MAX_WEIGHT;
+      }
+    }
+  }
+}
+
+void introduce_matriz(Grafo *g, int num_vertices, int matriz[N_VERTICES][N_VERTICES]){
+  for(int i = 0; i < num_vertices; i++){
+    for(int j = 0; j < num_vertices; j++){
+      g->matriz_adyacencia[i][j] = matriz[i][j];
+    }
+  }
+}
+
+void print_grafo(Grafo *g, int num_vertices){
+  printf("Grafo: \n");
+  for(int i = 0; i < num_vertices; i++){
+    printf("V %d: %s\n", g->vertices[i].id, g->vertices[i].nombre);
+  }
+
+  printf("\nMatriz de adyacencia:\n");
+  for(int i = 0; i < num_vertices; i++){
+    for(int j = 0; j < num_vertices; j++){
+      printf("%d ", g->matriz_adyacencia[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int distancia[N_VERTICES];
+int visitado[N_VERTICES];
+int anterior[N_VERTICES];
+
+void init_dij(int origen, int *d, int *v, int num_vertices){
+   for (int i = 0; i < num_vertices; i++)
+  {
+    distancia[i] = INF;
+    visitado[i] = 0;
+    anterior[i] = -1;
+  }
+  distancia[origen] = 0;
+}
+
+void imprimir_camino(int destino) {
+  if (anterior[destino] == -1) {
+    printf("%d", destino);
+    return;
+  }
+
+  imprimir_camino(anterior[destino]);
+  printf(" -> %d", destino);
+}
+
+
+void dijkstra(Grafo* g, int origen, int num_vertices){
+  init_dij(origen, distancia, visitado, num_vertices);
+
+  for(int i = 0; i < num_vertices -1; i++){
+    int u = -1;
+    int min_dist = INF;
+
+    for(int j = 0; j < num_vertices; j++){
+      if(!visitado[j] && distancia[j] < min_dist){
+        min_dist = distancia[j];
+        u = j;
+      }
+    }
+
+    if(u == -1) break;
+
+    visitado[u] = 1;
+
+    for(int v = 0; v < num_vertices; v++){
+      if(g->matriz_adyacencia[u][v] > 0 && !visitado[v]){
+        int nueva_dist = distancia[u] + g->matriz_adyacencia[u][v];
+        if(nueva_dist < distancia[v]){
+          distancia[v] = nueva_dist;
+          anterior[v] = u;
+        }
+      }
+    }
+  }
+ }
+void exportar_grafo_dot(Grafo* g, int num_vertices, const char* filename) {
+  FILE* file = fopen(filename, "w");
+  if (!file) {
+    perror("Error al abrir el archivo .dot");
+    return;
+  }
+
+  fprintf(file, "graph G {\n");
+
+  for (int i = 0; i < num_vertices; i++) {
+    fprintf(file, "  %d [label=\"%s\"];\n", i, g->vertices[i].nombre);
+  }
+
+  for (int i = 0; i < num_vertices; i++) {
+    for (int j = i + 1; j < num_vertices; j++) {
+      if (g->matriz_adyacencia[i][j] > 0) {
+        fprintf(file, "  %d -- %d [label=\"%d\"];\n", i, j, g->matriz_adyacencia[i][j]);
+      }
+    }
+  }
+
+  fprintf(file, "}\n");
+  fclose(file);
+}
+
+
+int main(){
+  Grafo g;
+
+  int m[N_VERTICES][N_VERTICES] = {
+    {0, 2, 6, 0, 0, 0, 0}, // 0
+    {2, 0, 0, 5, 0, 0, 0}, // 1
+    {6, 0, 0, 8, 0, 0, 0}, // 2
+    {0, 5, 8, 0, 10, 15, 0}, // 3
+    {0, 0, 0, 10, 0, 6, 2}, // 4
+    {0, 0, 0, 15, 6, 0, 6}, // 5
+    {0, 0, 0, 0, 2, 6, 0}, // 6
+  };
+
+  nombra_vertices(&g, N_VERTICES);
+  introduce_matriz(&g, N_VERTICES, m);
+  print_grafo(&g, N_VERTICES);
+
+  int origen = 0;
+  dijkstra(&g, origen, N_VERTICES);
+
+  imprimir_camino(6);
+  printf("Distancia total: %d\n", distancia[6]);
+
+  exportar_grafo_dot(&g, N_VERTICES, "grafo.dot");
+  printf("\nGrafo exportado a 'grafo.dot'. Usa Graphviz para visualizarlo.\n");
+  return 0;
+}
